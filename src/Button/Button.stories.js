6386f7af6f04c5b711b63e0b414cb53ce2b738d7
saijo-shota-biz <saijo.shota.biz@gmail.com>
2020-04-16 04:20:15 +0900

    add color button

--- src/Button/Button.stories.js
+++ src/Button/Button.stories.js
@@ -1,8 +1,34 @@
 import React from "react";
-import Button from "./Button";
+import Button, { ButtonColor } from "./Button";
+import { action } from "@storybook/addon-actions";
 
 export default {
   title: "Button",
 };
 
-export const base = () => <Button>Base Button</Button>;
+export const base = () => (
+  <Button onClick={action("clicked")}>Base Button</Button>
+);
+
+export const base_disabled = () => (
+  <Button disabled onClick={action("clicked")}>
+    Disabled Button
+  </Button>
+);
+
+export const colored = () => (
+  <>
+    <Button color={ButtonColor.MAIN} onClick={action("clicked")}>
+      Main Color Button
+    </Button>
+    <Button color={ButtonColor.DARK} onClick={action("clicked")}>
+      Dark Color Button
+    </Button>
+    <Button color={ButtonColor.LIGHT} onClick={action("clicked")}>
+      Light Color Button
+    </Button>
+    <Button color={ButtonColor.ACCENT} onClick={action("clicked")}>
+      Accent Color Button
+    </Button>
+  </>
+);
--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -3,15 +3,51 @@ import "./Button.scss";
 import PropTypes from "prop-types";
 
 export type ButtonProp = {
+  color: ButtonColor;
+  disabled: boolean;
   children: ReactNode;
+  onClick: () => void;
 };
 
-const Button: React.FC<ButtonProp> = ({ children }) => {
-  return <a className="el_btn">{children}</a>;
+export enum ButtonColor {
+  MAIN = "Main",
+  DARK = "Dark",
+  LIGHT = "Light",
+  ACCENT = "Accent",
+  DEFAULT = "Default",
+}
+
+const Button: React.FC<ButtonProp> = ({
+  color,
+  disabled,
+  children,
+  onClick,
+}) => {
+  const classes = [
+    "el_btn",
+    `el_btn__color${color}`,
+    disabled ? "el_btn__disabled" : "",
+  ].join(" ");
+
+  return (
+    <a className={classes} onClick={onClick}>
+      {children}
+    </a>
+  );
+};
+
+Button.defaultProps = {
+  color: ButtonColor.DEFAULT,
+  disabled: false,
+  children: "",
+  onClick: () => {},
 };
 
 Button.propTypes = {
+  color: PropTypes.oneOf(Object.keys(ButtonColor).map((e) => ButtonColor[e])),
+  disabled: PropTypes.bool,
   children: PropTypes.node.isRequired,
+  onClick: PropTypes.func,
 };
 
 export default Button;
