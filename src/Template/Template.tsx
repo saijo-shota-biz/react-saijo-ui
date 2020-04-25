8c34569be1792e0b09648325080f9081517dac36
saijo-shota-biz <60728259+saijo-shota-biz@users.noreply.github.com>
2020-04-26 04:49:20 +0900

    remove FooComponent add Template (#19)

---
+++ src/Template/Template.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Template.scss";
+// import PropTypes from "prop-types";
+
+export type TemplateProp = {};
+
+const Template: React.FC<TemplateProp> = () => {
+  return <></>;
+};
+
+Template.defaultProps = {};
+
+Template.propTypes = {};
+
+export default Template;
