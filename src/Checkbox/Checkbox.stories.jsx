231e18c36f6d06d67a96f2146c6a62472d4835e2
saijo-shota-biz <saijo.shota.biz@gmail.com>
2020-04-28 21:24:13 +0900

    #10 add checkbox

---
+++ src/Checkbox/Checkbox.stories.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import Checkbox from "./Checkbox";
+import { withKnobs } from "@storybook/addon-knobs";
+import { withInfo } from "@storybook/addon-info";
+import { useState } from "@storybook/addons";
+import CheckboxGroup from "./Checkbox";
+
+export default {
+  title: "Checkbox",
+  component: Checkbox,
+  decorators: [withKnobs, withInfo],
+};
+
+export const checkbox = () => {
+  const [state, setState] = useState({
+    ja: {
+      label: "日本語",
+      checked: false,
+    },
+    en: {
+      label: "English",
+      checked: false,
+      disabled: true,
+    },
+    fr: {
+      label: "Le français",
+      checked: true,
+      disabled: true,
+    },
+  });
+
+  return (
+    <>
+      <div>
+        {Object.entries(state).map(([key, value]) => (
+          <div key={key}>{`${value.label} : ${value.checked.toString()}`}</div>
+        ))}
+      </div>
+      <CheckboxGroup items={state} setItem={setState}></CheckboxGroup>
+    </>
+  );
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
 import Button from "./Button";
 import TextInput from "./TextInput";
 import Textarea from "./Textarea";
+import Checkbox from "./Checkbox";
 
 export { Button };
 export { TextInput };
 export { Textarea };
+export { Checkbox };
